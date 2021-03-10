12ba683999575d63a929ae491ca82b87783c1743
Olimpiaaxx <olimpiapater@gmail.com>
2021-03-10 11:49:49 +0000

    Trying to transfer quiz database into SQL

---
+++ SQL/SQLQuery2.sql
@@ -0,0 +1,51 @@
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three)
+VALUES (1, 'What is the capital of England?','London','Bristol','Slough','Windsor');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (2, 'What is the capital of Uganda?','Kampala','Lome','Bangkok','Colombo', 'capital');
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (3, 'What is the capital of Slovakia?','Bratislava','Ljubljana','Belgrade','Riyadh', 'capital');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (4, 'What is the capital of Peru?','Lima','Manila','Melekeok','San Salvador', 'capital');
+
+
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (5, 'Where is La Sagrada Familia?','Barcelona','Venice','Milan','Madrid', 'attractions');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (6, 'Where is CN Tower?','Toronto','Montreal','Vancouver','Ottawa', 'attractions');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (7, 'Where is Statue of Liberty?','New York','New Jersey','Los Angeles','San Francisco', 'attractions');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (8, 'Where is Burj Khalifa?','Dubai','Abu Dhabi','Singapore','Tokyo', 'attractions');
+
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (9, 'Where are the famous PIEROGI from?','Poland','Czech Republic','Austria','Germany', 'food');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (10, 'Where is the famous SCHNITZEL from?','Austria','Hungary','Russia','Poland', 'food');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (11, 'Where is the famous POUTINE from?','Canada','Australia','USA','Brazil', 'food');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (12, 'Where is the famous PAELLA from?','Spain','Italy','England','France', 'food');
+
+
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (13, 'Where is Victoria Falls located?','Zambia','Iceland','Uganda','Australia', 'nature');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (14, 'Where is Mount Kilimanjaro located?','Tanzania','Japan','Russia','Thailand', 'nature');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (15, 'Where is Pamukkale located?','Turkey','Italy','Iceland','Greece', 'nature');
+
+INSERT INTO Quiz(QuestionID, Question, Correct_Answer, Wrong_Answer_One, Wrong_Answer_Two, Wrong_Answer_Three, Category)
+VALUES (16, 'Where is Banff National Park located?','Canada','Italy','New Zealand','France', 'nature');
